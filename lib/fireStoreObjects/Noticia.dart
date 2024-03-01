dacdc152e4daa21429cd4d4b1324e79446a1b75b
carlosgr1992 <carlos.gr1992@gmail.com>
2024-03-02 00:55:43 +0100

    Creada la clase Noticia

---
+++ lib/fireStoreObjects/Noticia.dart
@@ -0,0 +1,17 @@
+
+class Noticia {
+
+  final String titulo;
+  final String descripcion;
+  final String urlImagen;
+  final String urlNoticia;
+
+  Noticia({
+    required this.titulo,
+    required this.descripcion,
+    required this.urlImagen,
+    required this.urlNoticia
+});
+
+
+}
