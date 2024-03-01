7c94cdce71c627cab40fd42d089618bb27de31da
carlosgr1992 <carlos.gr1992@gmail.com>
2024-03-01 21:17:25 +0100

    Conectado proyecto a FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'WorldNews.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(WorldNews());
 
 }
