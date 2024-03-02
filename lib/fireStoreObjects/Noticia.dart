1d0fa1546cc9a3420ea41afb6d5610d50c334c1f
carlosgr1992 <carlos.gr1992@gmail.com>
2024-03-02 16:58:27 +0100

    Añadida URL a la cada noticia para poder verla completa

--- lib/fireStoreObjects/Noticia.dart
+++ lib/fireStoreObjects/Noticia.dart
@@ -5,19 +5,21 @@ class Noticia {
   final String descripcion;
   final String urlImagen;
   final String urlNoticia;
+  final String contenido;
 
   Noticia({
     required this.titulo,
     required this.descripcion,
     required this.urlImagen,
-    required this.urlNoticia
+    required this.urlNoticia,
+    required this.contenido,
 });
 
   Noticia.fromJson(Map<String, dynamic> json)
       : titulo = json['title'] ?? 'Título no disponible',
         descripcion = json['description'] ?? 'Descripción no disponible',
         urlImagen = json['urlToImage'] ?? '',
-        urlNoticia = json['url'] ?? '';
-
+        urlNoticia = json['url'] ?? '',
+        contenido = json['content'] ?? '';
 
 }
--- lib/onBoarding/DetalleNoticiaView.dart
+++ lib/onBoarding/DetalleNoticiaView.dart
@@ -2,46 +2,51 @@ import 'package:flutter/material.dart';
 import '../fireStoreObjects/Noticia.dart';
 
 class DetalleNoticiaView extends StatelessWidget {
-  final Noticia? noticia; // Lo ponemos a posible null para el constructor del WorldNews poner tener la ruta.
+  final Noticia? noticia;
 
-  DetalleNoticiaView({this.noticia}); // Hago el parámetro opcional con '?'
+  DetalleNoticiaView({this.noticia});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(noticia?.titulo ?? 'Detalle de Noticia'), // Acceso opcional a la propiedad titulo
+        title: Text(noticia?.titulo ?? 'Detalle de Noticia'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-              noticia?.urlImagen.isNotEmpty ?? false ? noticia!.urlImagen : 'images/imagenPredeterminada.jpeg',
-              fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) {
-                return Image.asset('images/imagenPredeterminada.jpeg', fit: BoxFit.cover);
-              },
+      body: ListView(
+        children: [
+          Image.network(
+            noticia?.urlImagen.isNotEmpty ?? false ? noticia!.urlImagen : 'images/imagenPredeterminada.jpeg',
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return Image.asset('images/imagenPredeterminada.jpeg', fit: BoxFit.cover);
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  noticia?.titulo ?? 'Titulo no disponible',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  noticia?.descripcion ?? 'Descripción no disponible',
+                  style: TextStyle(fontSize: 18),
+                ),
+                SizedBox(height: 8),
+                SelectableText(
+                  '${noticia?.urlNoticia}',
+                  style: TextStyle(color: Colors.blueAccent, decoration: TextDecoration.underline),
+                  onTap: () {
+                    // Añadir movimiento al enlace (hacer más adelante, muy complejo)
+                  },
+                ),
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    noticia?.titulo ?? 'Titulo no disponible', // Acceso opcional al titulo
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    noticia?.descripcion ?? 'Descripción no disponible', // Acceso opcional a la descripcion
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
