62600e03141dcf6f2b00c0905ae330d86abdbbad
carlosgr1992 <132667387+carlosgr1992@users.noreply.github.com>
2025-06-04 11:18:51 +0200

    Rename singletone directory and update imports

--- lib/onBoarding/Splash.dart
+++ lib/onBoarding/Splash.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../fireStoreObjects/FbUsuario.dart';
-import '../singletone/DataHolder.dart';
+import '../singleton/DataHolder.dart';
 
 class Splash extends StatefulWidget {
   @override
